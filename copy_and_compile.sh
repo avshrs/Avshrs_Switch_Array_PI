815f6c9b480c0d6bb207e80bfb18de05673635b5
avshrs <avshrs@gmail.com>
2021-11-27 12:22:09 +0100

    cmake finished

--- copy_and_compile.sh
+++ copy_and_compile.sh
@@ -1,2 +1,2 @@
 scp -r /home/seba/Documents/RPI_Zero_mcp pi@192.168.1.21:/home/pi/ 
-ssh pi@192.168.1.21 "cd /home/pi/RPI_Zero_mcp; killall a.out; g++ -pthread -lmosquittopp -lwiringPi -I. -L. main.cpp  MCP_Mosquitto.cpp mqtt.cpp MCPdev.cpp MCP_Settings.cpp MCP_Manager.cpp socket_server.cpp Settings_server.cpp && ./a.out"
+ssh pi@192.168.1.21 "cd /home/pi/RPI_Zero_mcp; killall a.out; g++ -pthread -lmosquittopp -lwiringPi -Iinclude/. -Linclude/. src/main.cpp  src/MCP_Mosquitto.cpp src/mqtt.cpp src/MCPdev.cpp src/MCP_Settings.cpp src/MCP_Manager.cpp src/socket_server.cpp src/Settings_server.cpp && ./a.out"
