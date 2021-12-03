b073fca1848dcde979e7c8b5e6e0b7ca3c77a252
avshrs <avshrs@gmail.com>
2021-12-03 22:18:19 +0100

    up

--- src/MCP_i2c.cpp
+++ src/MCP_i2c.cpp
@@ -33,11 +33,11 @@ uint8_t MCP_i2c::readByte(){
 
 void MCP_i2c::writeBytes(char * buffer){
     if (write(file_i2c, buffer, sizeof(buffer)) != sizeof(buffer)) {
-            printf("Failed to write to the i2c bus.\n");
+            printf("2c bus.\n");
     }
 }
-void MCP_i2c::writeByte(uint8_t buffer){
-    
+void MCP_i2c::writeByte(uint8_t buffer_){
+    char *buffer = &buffer_;
     if (write(file_i2c, &buffer, sizeof(buffer)) != sizeof(buffer)) {
             printf("Failed to write to the i2c bus.\n");
     }    
