13f1088eac9b5b2223eaf7f2155c817d5c1d66d7
avshrs <avshrs@gmail.com>
2021-12-04 12:35:20 +0100

    update - delay added

--- src/MCP_Manager.cpp
+++ src/MCP_Manager.cpp
@@ -63,6 +63,7 @@ void MCP_Manager::write_output(uint8_t out, bool state)
 void MCP_Manager::scan_all_io(){
     for(int i = 0; i < IN_RANGE ; i++){
         scan_in_and_set_out(i);
+        usleep(1000);
     }
 }
 
