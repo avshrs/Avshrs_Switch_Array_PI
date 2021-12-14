2673e5d17e417a0db8b3375f85e0300e06bb2e61
avshrs <avshrs@gmail.com>
2021-12-14 12:21:44 +0100

    up

--- include/MCP_i2c.h
+++ include/MCP_i2c.h
@@ -16,8 +16,12 @@ class MCP_i2c{
     
     public:
         void i2c_init(std::string filename, uint8_t address_);
+        
         uint8_t readByte(uint8_t reg);
         void writeByte(uint8_t side, uint8_t buffer_);
         
+        uint16_t readWord(uint8_t reg);
+        void writeWord(uint8_t reg, uint16_t buffer_);
+
 };
 
--- src/MCP_i2c.cpp
+++ src/MCP_i2c.cpp
@@ -11,7 +11,6 @@ extern "C" {
 #include "MCP_i2c.h"
 #include <stdlib.h>
 #include <iostream>
-
 #include <iomanip>
 #include <ctime>
 
@@ -22,13 +21,13 @@ void MCP_i2c::i2c_init(std::string filename, uint8_t address){
             auto t = std::time(nullptr);
             auto tm = *std::localtime(&t);
             std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
-            printf("Failed to open the i2c bus \n");
+            std::cout << "Failed to Open the i2c bus"<< std::endl;
     }
     if (ioctl(file_i2c, I2C_SLAVE, address) < 0) {
             auto t = std::time(nullptr);
             auto tm = *std::localtime(&t);
             std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
-            printf("Failed to acquire bus access and/or talk to slave.\n");
+            std::cout << "Failed to acquire bus access and/or talk to slave." << std::endl;
     }
 }
 
@@ -37,8 +36,9 @@ uint8_t MCP_i2c::readByte(uint8_t reg){
     if (res<0){
         auto t = std::time(nullptr);
         auto tm = *std::localtime(&t);
-        std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ") << std::hex << reg << " | " << "i2c-error: " << std::dec <<res;
-        printf("Failed to read from the i2c bus.\n");
+        std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
+        std::cout << std::hex << reg << " | " << "i2c-error: " << std::dec <<res;
+        std::cout << "Failed to Read from the i2c bus."<< std::endl;
         return 0;
     }
     else {
@@ -47,8 +47,39 @@ uint8_t MCP_i2c::readByte(uint8_t reg){
     }
 }
 
+uint16_t MCP_i2c::readWord(uint8_t reg){
+    auto res = i2c_smbus_read_word_data(file_i2c, reg);
+    if (res<0){
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
+        std::cout << std::hex << reg << " | " << "i2c-error: " << std::dec <<res;
+        std::cout << "Failed to Read from the i2c bus." << std::endl;
+        return 0;
+    }
+    else {
+        return static_cast<uint16_t>(res);
+    }
+}
 
-void MCP_i2c::writeByte(uint8_t side, uint8_t buffer_){
+void MCP_i2c::writeByte(uint8_t reg, uint8_t buffer_){
     unsigned short buffer = static_cast<uint8_t>(buffer_);
-    i2c_smbus_write_byte_data(file_i2c, side, buffer_);
+    auto res = i2c_smbus_write_byte_data(file_i2c, reg, buffer_);
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
+    std::cout << std::hex << reg << " | " << "i2c-error: " << std::dec <<res;
+    std::cout << "Failed to Write to the i2c bus." << std::endl;
+}
+
+void MCP_i2c::writeWord(uint8_t reg, uint16_t buffer_){
+    unsigned short buffer = static_cast<uint8_t>(buffer_);
+    auto res = i2c_smbus_write_word_data(file_i2c, reg, buffer_);
+
+    if (res<0){
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S | ");
+        std::cout << std::hex << reg << " | " << "i2c-error: " << std::dec <<res;
+        std::cout << "Failed to Write to the i2c bus." << std::endl;
 }
