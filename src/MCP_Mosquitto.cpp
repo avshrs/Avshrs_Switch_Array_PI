0d7b6461540f8cd59b59dd9c601aa4eaadb3d4cf
avshrs <avshrs@gmail.com>
2021-12-17 22:39:09 +0100

    mosq update

--- src/MCP_Mosquitto.cpp
+++ src/MCP_Mosquitto.cpp
@@ -3,7 +3,7 @@
 
 
 void MCP_Mosquitto::mos_connect(){
-    mqtt_client *iot_client;
+    
     int rc;
 
     char client_id[] = CLIENT_ID;
@@ -12,26 +12,24 @@ void MCP_Mosquitto::mos_connect(){
 
     mosqpp::lib_init();
 
-
-    iot_client = new mqtt_client(client_id, host, port);
-    iot_client->register_mcp_manager(mcp_manager);
-    iot_client->subscribe(NULL, "MCP_Array");  // Main device topic - Online 
-    //iot_client->publish(NULL, "MCP_Array");  // Main device topic - Online 
+    mqtt_client iot_client(client_id, host, port);
+    iot_client.register_mcp_manager(mcp_manager);
+    iot_client.subscribe(NULL, "MCP_Array");  // Main device topic - Online 
 
     for(int i = 0; i<64; i++){
         std::string pub = "MCP_OUT_P_";
         std::string sub = "MCP_OUT_S_";
         sub += std::to_string(i);
         pub += std::to_string(i);
-        iot_client->publish(NULL, pub.c_str());
-        iot_client->subscribe(NULL, sub.c_str());
+        iot_client.publish(NULL, pub.c_str());
+        iot_client.subscribe(NULL, sub.c_str());
         usleep(10000);
     }
     std::string mcp_array ="MCP_Array";
     std::string mcp_array_msg ="Online";
     while(1)
     {
-        rc = iot_client->loop();
+        rc = iot_client.loop();
        
     }
     mosqpp::lib_cleanup();
