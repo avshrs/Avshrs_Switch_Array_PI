82f7f9d8ce6e324da3fe9c226be93bb0b99666f0
avshrs <avshrs@gmail.com>
2021-12-22 23:59:38 +0100

    up

--- include/mqtt.h
+++ include/mqtt.h
@@ -21,7 +21,7 @@ public:
     void register_mcp_manager(MCP_Manager *mcp_manager_);
     void on_connect(int rc);
     void on_error();
-    void on_disconnect(int rc)
+    void on_disconnect(int rc);
     void on_message(const struct mosquitto_message *message);
     void on_subscribe(int mid, int qos_count, const int *granted_qos);
 };
