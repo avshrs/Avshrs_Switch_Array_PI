f432b52edebcb33aa02926517be13b8ac8bd8985
avshrs <avshrs@gmail.com>
2021-12-23 22:27:53 +0100

    up

--- src/MCP_Mosquitto.cpp
+++ src/MCP_Mosquitto.cpp
@@ -1,18 +1,21 @@
 #include "MCP_Mosquitto.h"
 
-char client_id[] = "SwitchMatrix";
-char host[] = "192.168.1.96";
-int port = 1883;
-mqtt_client iot_client(client_id, host, port);
+
 
 void MCP_Mosquitto::mos_connect(){
-
+    char client_id[] = "SwitchMatrix";
+    char host[] = "192.168.1.96";
+    int port = 1883;
 
     mosqpp::lib_init();
-       
+
+    mqtt_client iot_client(client_id, host, port);
+
     iot_client.reconnect_delay_set(10, 100, true);
     iot_client.register_mcp_manager(mcp_manager);
+    
     rc = iot_client.loop_forever();
+    
     std::cout<<"loop exited"<< std::endl;
     mosqpp::lib_cleanup();
 }
